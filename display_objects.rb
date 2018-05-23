709e090c791977af33e0500ef4c01c00daef6b14
atxrenegade <harleighabel@gmail.com>
2018-05-23 01:47:41 -0500

    create and stub out #output_data

---
+++ display_objects.rb
@@ -0,0 +1,16 @@
+class DisplayObject(data_hash)
+ 	#what does the data look like coming from Scraper?
+	#Output will include 2-5 of the following details :title, :key_sequence or #:command, :function, :description (only on some commands not on shortcuts, these will include index numbers to access these details)
+def output_data(title, shortcut_command_hash)
+	puts "******************************************************\n"
+	puts "\t\t\t#{title.upcase}\n"
+	puts "******************************************************\n\n"
+	puts shortcut_command_hash.each | category |
+		if heading != nil
+			puts "\t#{heading.upcase}"
+			
+
+end
+
+
+end
