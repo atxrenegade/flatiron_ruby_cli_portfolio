dbe210ccaef4ec17959b2305f6583d357eed44ea
atxrenegade <harleighabel@gmail.com>
2018-05-22 23:55:45 -0500

    create seperate document for Scraper class

--- lib/web_dev_cheatsheets.rb
+++ lib/web_dev_cheatsheets.rb
@@ -59,37 +59,6 @@ end
 		end
 	end
 
-	class Scraper
-		def initialize(user_selection)
-			case user_selection
-			when 1.1
-				puts "  Im choosing a random category from Mac/Linux OS relevant shortcuts and\n"
-				puts "  and commands and scraping it for the random shortcut of the day!\n"
-			when 1.2
-				puts "  Im choosing a random category from WINDOWS OS relevant shortcuts and\n"
-				puts "  and commands and scraping it for the random shortcut of the day!\n"
-			when 1.3
-				puts "  Im choosing a random category from ALL OF THE ABOVE relevant shortcuts and\n"
-				puts "  and commands and scraping it for the random shortcut of the day!\n"
-			when 2
-				puts "  Scraping Github COMMANDS!\n"
-			when 3
-				puts "  Scraping Atom SHORTCUTS!\n"
-			when 4
-				puts "  Scraping Pry COMMANDS!\n"
-			when 5
-				puts "  Scraping Bash/Shell COMMANDS!\n"
-			when 6
-				puts "  Scraping COMMAND (CLI for WINDOWS!)\n"
-			when 7
-				puts "  Scraping Mac OS Keyboard SHORTCUTS!\n"
-			when 8
-				puts "  Scraping Windows Keyboard SHORTCUTS!\n"
-			end
-		end
-		puts "\n\n"
-	end
-
 	class Daily_Shortcut
 		def initialize
 			daily_shortcut_greeting
---
+++ scraper.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+require 'nokogiri'
+
+class Scraper
+	def initialize(user_selection)
+		case user_selection
+		when 1.1
+			puts "  Im choosing a random category from Mac/Linux OS relevant shortcuts and\n"
+			puts "  and commands and scraping it for the random shortcut of the day!\n"
+		when 1.2
+			puts "  Im choosing a random category from WINDOWS OS relevant shortcuts and\n"
+			puts "  and commands and scraping it for the random shortcut of the day!\n"
+		when 1.3
+			puts "  Im choosing a random category from ALL OF THE ABOVE relevant shortcuts and\n"
+			puts "  and commands and scraping it for the random shortcut of the day!\n"
+		when 2
+			puts "  Scraping Github COMMANDS!\n"
+		when 3
+			puts "  Scraping Atom SHORTCUTS!\n"
+		when 4
+			puts "  Scraping Pry COMMANDS!\n"
+		when 5
+			puts "  Scraping Bash/Shell COMMANDS!\n"
+		when 6
+			puts "  Scraping COMMAND (CLI for WINDOWS!)\n"
+		when 7
+			puts "  Scraping Mac OS Keyboard SHORTCUTS!\n"
+		when 8
+			puts "  Scraping Windows Keyboard SHORTCUTS!\n"
+		end
+	end
+	puts "\n\n"
+end
