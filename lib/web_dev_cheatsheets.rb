50bf7ff49ec0c85c15da4951d6b698651b49d6d1
atxrenegade <harleighabel@gmail.com>
2018-05-22 19:35:08 -0500

    create new classes and break down methods into appropriate classes

--- lib/web_dev_cheatsheets.rb
+++ lib/web_dev_cheatsheets.rb
@@ -1,64 +1,31 @@
 require "web_dev_cheatsheets/version"
 
-	class CLI_interface
-		attr_accessor :first_run
-
-		def initialize(first_run=true)
-			 first_run ? welcome : main_menu
-			 #correct program so that intro does not execute after first run through program
-
-		end
-
-		def welcome
-			#can this be in the executable and removed from the class since it's a one off?
-			puts "\n\n\t\t\tWELCOME TO THE\n ******  LAZY PROGRAMMER's Shortcuts and Cheatsheets  ******\n\n"
-
-			puts "  Shortcuts and Cheatsheets is a cli utility gem designed\n"
-			puts "  as a quick reference to a web developer’s most commonly\n"
-			puts "  used tools. Think of it as a Swiss army knife for web developers\n"
-			puts "  and web dev students. With so many languages, commands, shortcuts\n"
-			puts "  and tools to memorize, you can now access a quick digital\n"
-			puts "  reference without having shuffle through pages of cheatsheets,\n"
-			puts "  or drown in stack overflow posts.\n\n"
-			puts "  Being familiar with shortcuts and important commands speeds up\n"
-			puts "  your workflow and increases your productivity as a programmer.\n"
-			puts "  This program does not to include an exhaustive list of all\n"
-			puts "  available commands but focuses on the most commonly used\n"
-			puts "  shortcuts and commands you will need as a developer.\n\n"
-			puts "\n\n  Press the ENTER key to continue....."
-			any_key = gets
-			if any_key.include?("\n")
-				main_menu
-			end
+	class Menus
+		def initialize
+			main_menu
 		end
 
 		def main_menu
-			puts"\n*********************************\n"
-			puts "**********  MAIN MENU  **********\n"
-			puts "*********************************\n\n"
-			puts "  1. Shortcut of the Day\n  2. Github COMMANDS \n  3. Atom SHORTCUTS\n"
-			puts "  4. Pry COMMANDS\n  5. Bash/Shell COMMANDS (CLI for linux and UNIX)\n"
-			puts "  6. COMMAND (CLI for windows)\n  7. Mac OS keyboard SHORTCUTS\n"
-			puts "  8. Windows keyboard SHORTCUTS\n  X. EXIT\n\n"
-			puts "  Please select the number for the command you would\n  like to execute?\n\t"
-			input = gets.strip!
-			if input == "X" || input == "x"
-				exit
-			elsif input == "1"
-				daily_shortcut_greeting
-			else
-				Scraper.new(input.to_i)
+			def main_menu
+				puts"\n*********************************\n"
+				puts "**********  MAIN MENU  **********\n"
+				puts "*********************************\n\n"
+				puts "  1. Shortcut of the Day\n  2. Github COMMANDS \n  3. Atom SHORTCUTS\n"
+				puts "  4. Pry COMMANDS\n  5. Bash/Shell COMMANDS (CLI for linux and UNIX)\n"
+				puts "  6. COMMAND (CLI for windows)\n  7. Mac OS keyboard SHORTCUTS\n"
+				puts "  8. Windows keyboard SHORTCUTS\n  X. EXIT\n\n"
+				puts "  Please select the number for the command you would\n  like to execute?\n\t"
+				input = gets.strip!
+				if input == "X" || input == "x"
+					exit
+				elsif input == "1"
+					daily_shortcut_greeting
+				else
+					Scraper.new(input.to_i)
+				end
 			end
 		end
 
-		def daily_shortcut_greeting
-			puts "\n  WELCOME TO SHORTCUTS!\n"
-			puts "  This feature is designed to familiarize you with a\n"
-			puts "  single randomly chosen shortcut. To make this the\n"
-			puts "  most relevant to you can you tell me which operating\n"
-			puts "  systems you are using?\n"
-			daily_shortcut_submenu
-		end
 		def daily_shortcut_submenu
 			puts "  1. Mac/Linux\n  2. Windows\n  3. All of the Above\n"
 			puts "  MM. Main Menu\n  X. EXIT"
@@ -89,11 +56,6 @@ require "web_dev_cheatsheets/version"
 			end
 		end
 
-		def exit
-			puts "\n  Thanks for using the LAZY PROGRAMMER's shortcuts and cheatsheets!"
-			puts "  See you again soon!\n"
-		end
-	end
 
 	class Scraper
 		def initialize(user_selection)
@@ -125,4 +87,44 @@ require "web_dev_cheatsheets/version"
 		end
 		puts "\n\n"
 		CLI_interface.new(false)
+	end
+
+	class Daily_Shortcut
+		def daily_shortcut_greeting
+			puts "\n  WELCOME TO SHORTCUTS!\n"
+			puts "  This feature is designed to familiarize you with a\n"
+			puts "  single randomly chosen shortcut. To make this the\n"
+			puts "  most relevant to you can you tell me which operating\n"
+			puts "  systems you are using?\n"
+			daily_shortcut_submenu
+		end
+	end
+
+	class Greeting
+		def welcome
+			#can this be in the executable and removed from the class since it's a one off?
+			puts "\n\n\t\t\tWELCOME TO THE\n ******  LAZY PROGRAMMER's Shortcuts and Cheatsheets  ******\n\n"
+
+			puts "  Shortcuts and Cheatsheets is a cli utility gem designed\n"
+			puts "  as a quick reference to a web developer’s most commonly\n"
+			puts "  used tools. Think of it as a Swiss army knife for web developers\n"
+			puts "  and web dev students. With so many languages, commands, shortcuts\n"
+			puts "  and tools to memorize, you can now access a quick digital\n"
+			puts "  reference without having shuffle through pages of cheatsheets,\n"
+			puts "  or drown in stack overflow posts.\n\n"
+			puts "  Being familiar with shortcuts and important commands speeds up\n"
+			puts "  your workflow and increases your productivity as a programmer.\n"
+			puts "  This program does not to include an exhaustive list of all\n"
+			puts "  available commands but focuses on the most commonly used\n"
+			puts "  shortcuts and commands you will need as a developer.\n\n"
+			puts "\n\n  Press the ENTER key to continue....."
+			any_key = gets
+			if any_key.include?("\n")
+				Menu.main_menu
+			end
+
+			def exit
+				puts "\n  Thanks for using the LAZY PROGRAMMER's shortcuts and cheatsheets!"
+				puts "  See you again soon!\n"
+			end
 	end
