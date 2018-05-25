b5636f36362da0829643d80918b695cca186d8ae
atxrenegade <harleighabel@gmail.com>
2018-05-25 01:18:19 -0500

    add Command#all class method

--- lib/command.rb
+++ lib/command.rb
@@ -1,9 +1,17 @@
 class Command
-attr_accessor :command_name, :function, :description, :node, :commands
+attr_accessor :command_name, :function, :description, :node
 
     @@all = []
 
     def initialize(node_name, command_name, function, description= nil)
-        
+		@node = node
+        @command_name = command_name
+        @function = function
+        @description = description
+        @@all << self
     end
+
+	def self.all
+        @@all
+    end  
 end
