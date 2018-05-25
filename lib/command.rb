5944674274b695ba64f78900b76cc0e2771e5945
atxrenegade <harleighabel@gmail.com>
2018-05-25 01:19:38 -0500

    add Command#new_from_node

--- lib/command.rb
+++ lib/command.rb
@@ -11,7 +11,19 @@ attr_accessor :command_name, :function, :description, :node
         @@all << self
     end
 
+	def new_from_node(tool, data_array)
+        command_name = data_array[1]
+        function = data_array[2]
+        if data_array[3] != nil
+            @description = data_array[3]
+        else
+            @description = nil
+        end
+        node = ToolNode.new(data_array[0])
+        command = Command.new(node, command_name, function, description)  
+    end
+
 	def self.all
         @@all
-    end  
+    end
 end
