00d0b9955375fb51498c42170a2ec98c2e7a58ee
atxrenegade <harleighabel@gmail.com>
2018-06-22 03:01:46 -0600

    add to Module#create_if_none

--- lib/module.rb
+++ lib/module.rb
@@ -10,7 +10,11 @@ class module
 	end
 
 	def create_if_none(name)
+		@@all.detect {|instance| name == instance.name}
+	end
 
+	def add_command(command)
+		@@command << command
 	end
 
 	def self.commands
