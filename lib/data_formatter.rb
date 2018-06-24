8ab3012aff78adad954cdd2cd8dd4047424f92cd
atxrenegade <harleighabel@gmail.com>
2018-06-23 22:57:58 -0600

    add to DataFormatter#format_atom

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -10,14 +10,14 @@ class DataFormatter
 			format_github(data_array)
 		else tool == "Atom"
 			format_atom(data_array)
-		#add future pages options here	
+		#add future pages options here
 		end
 	end
 
 	def format_github
-
-
-
+		github = Tool.create("Git Hub")
+		#how do i parse the array to create modules with commands,
+		#functions, and details
 	end
 
 	def format_atom
