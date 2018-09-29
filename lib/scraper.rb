b9b6906cbf5a56c37c5b94e4c0e12f0087787ec2
atxrenegade <harleighabel@gmail.com>
2018-09-29 14:55:35 -0500

    work on scraper class #scrape_atom, correct typo in cli_interface

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,24 +17,18 @@ class Scraper
 		end
 	end
 
-	def scrape_github#to be deleted
-		github_doc = Nokogiri::HTML(open("http://www.yolinux.com/TUTORIALS/Git-commands.html"))
-		data_string = github_doc.css("html body div.container-fluid div.yo-row div#content table.box tbody")
-
- 		#test github = Scraper.new("2").scrape_github
-
-		FormatData.new(github, data_array)
-	end
-
 	def scrape_ruby
 
 	end
-
 	def scrape_bash
 
 	end
 
 	def scrape_atom
-
+		binding.pry
+		atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
+		binding.pry
+		#data_string = github_doc.css("html body div.container-fluid div.yo-row div#content table.box tbody")
+		#FormatData.new(github, data_array)
 	end
 end
