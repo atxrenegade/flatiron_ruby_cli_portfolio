b652f61324a70516dd757bd9f724875b0f038a54
atxrenegade <harleighabel@gmail.com>
2018-09-29 15:41:55 -0500

    complete #scrape_atom

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,10 +26,14 @@ class Scraper
 	end
 
 	def scrape_atom
-		binding.pry
-		atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
-		#binding.pry
-		#data_string = github_doc.css("html body div.container-fluid div.yo-row div#content table.box tbody")
-		#FormatData.new(github, data_array)
+		if shortcut_string.empty?
+			binding.pry
+			atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
+			shortcut_string = atom_doc.css("html body div div #js-repo-pjax-container div div #readme div article.markdown-body.entry-content table tbody").children.text
+
+			format_data(shortcut_string)
+		else
+			#submenu list all or search shortcuts
+		end	
 	end
 end
