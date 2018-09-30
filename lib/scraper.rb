0c15665e9f0b92206b4b0fa86648f21feac012c3
atxrenegade <harleighabel@gmail.com>
2018-09-29 19:34:22 -0500

    update scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,10 @@
-
 class Scraper
 	def initialize(user_input)
-		@user_input = user_input
 		self.selector
 	end
 
-	def selector
-		if @user_input == 1
-			puts "  Scraping Atom SHORTCUTS!\n"
-			scrape_atom
-		elsif @user_input == 2
-			puts "  Scraping Bash COMMANDS!\n"
-			scrape_bash
-		else
-			puts "  Scraping Ruby METHODS!\n"
-			scrape_atom
-		end
-	end
-
 	def scrape_atom
-		if @shortcut_string == nil
-			atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
-			@shortcut_string = atom_doc.css("html body div div #js-repo-pjax-container div div #readme div article.markdown-body.entry-content table tbody").children.text
-			binding.pry
-			#@shortcut_string.format_data
-		else
-			#submenu list all or search shortcuts
-		end
+		atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
+		@shortcut_string = atom_doc.css("html body div div #js-repo-pjax-container div div #readme div article.markdown-body.entry-content table tbody").children.text
 	end
 end
--- lib/search_shortcuts.rb
+++
@@ -1,3 +0,0 @@
-class SearchShortcuts
-
-end
