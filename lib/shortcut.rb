619e8985ff1e2ce789fb7f2936ee60816ad7e0f1
atxrenegade <harleighabel@gmail.com>
2018-09-29 19:31:11 -0500

    add functions to Shortcut class

--- lib/shortcut.rb
+++ lib/shortcut.rb
@@ -1,4 +1,25 @@
 class Shortcut
+	attr_accessor :name, :description, :shortcut_key
 	def initialize
-	end	
+	end
+
+	def create_shortcut_from_data_array(data_array)
+
+	end
+
+	def all_shortcuts
+
+	end
+
+	def all_descriptions
+
+	end
+
+	def find_by_name
+
+	end
+
+	def all
+
+	end
 end
