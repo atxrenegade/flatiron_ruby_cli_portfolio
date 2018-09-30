529ea579f9a183de0a59ba5dc19dccee41e65609
atxrenegade <harleighabel@gmail.com>
2018-09-29 19:24:18 -0500

    create functions for shortcut_key class

--- lib/shortcut.rb
+++ lib/shortcut.rb
@@ -1,3 +1,4 @@
 class Shortcut
-
+	def initialize
+	end	
 end
--- lib/shortcut_key.rb
+++ lib/shortcut_key.rb
@@ -1,3 +1,23 @@
 class ShortcutKey
+	attr_accessor :mac, :linux, :windows, :shortcut
+	def initialize
+	end
+
+	def create_from_data_array(data_array)
+		#creates new shortcuts from data array
+	end
+
+	def all_mac
+		#lists all mac commands
+
+	end
+
+	def all_windows
+		#lists all windows commands
+	end
+
+	def all_linux
+		#lists all linux commands
+	end
 
 end
