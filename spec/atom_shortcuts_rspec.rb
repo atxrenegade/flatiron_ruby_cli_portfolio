c9243d9853fce8436a6fd06af2e162fbb3d3ec6f
atxrenegade <harleighabel@gmail.com>
2018-10-01 14:18:03 -0500

    update rspec file

--- spec/atom_shortcuts_rspec.rb
+++ spec/atom_shortcuts_rspec.rb
@@ -1,6 +1,6 @@
-RSpec.describe WebDevCheatsheets do
+RSpec.describe AtomShortcuts do
   it "has a version number" do
-    expect(WebDevCheatsheets::VERSION).not_to be nil
+    expect(AtomShortcuts::VERSION).not_to be nil
   end
 
   it "does something useful" do
