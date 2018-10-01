db228f76aa5e4e0f9b46514285cafa2f669ab3d2
atxrenegade <harleighabel@gmail.com>
2018-10-01 15:22:47 -0500

    eliminate unnecessary variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,10 @@
 class Scraper
+
 	ATOM_PATH = "https://github.com/nwinkler/atom-keyboard-shortcuts"
 
 	def scrape_atom
 		atom_doc = Nokogiri::HTML(open(ATOM_PATH))
 		shortcut_string = atom_doc.css("html body div div #js-repo-pjax-container div div #readme div article.markdown-body.entry-content table tbody").text
-		data = DataFormatter.new.create_data_array_from_scraper(shortcut_string)
+		DataFormatter.new.create_data_array_from_scraper(shortcut_string)
 	end
 end
