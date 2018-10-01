05b7738be9a37320a3d396fe19383dfc70ef3ddb
atxrenegade <harleighabel@gmail.com>
2018-10-01 17:49:26 -0500

    debug DataFormatter#create_objects_from_data

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -1,8 +1,14 @@
 class DataFormatter
-	
+
 	def create_data_array_from_scraper(data)
 		shortcut_array = data.split("Block Travel").first.split("\n")
-		shortcut_array = shortcut_array.reject { |item| item.nil? || item == ''}.each_slice(5).to_a
+		@shortcut_array = shortcut_array.reject { |item| item.nil? || item == ''}.each_slice(5).to_a
+		create_objects_from_data
+	end
+
+	def create_objects_from_data
+		binding.pry
+		@shortcut_array.each {|shortcut| Shortcut.new_from_data_array(shortcut)}
 		binding.pry
 	end
 end
