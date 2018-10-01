be8af5ca8f3be7666e19268856b06e102442d090
atxrenegade <harleighabel@gmail.com>
2018-10-01 15:18:48 -0500

    remove Scraper #initialize add constant for path variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,8 @@
 class Scraper
-	def initialize
-	end
+	ATOM_PATH = "https://github.com/nwinkler/atom-keyboard-shortcuts"
 
 	def scrape_atom
-		atom_doc = Nokogiri::HTML(open("https://github.com/nwinkler/atom-keyboard-shortcuts"))
+		atom_doc = Nokogiri::HTML(open(ATOM_PATH))
 		shortcut_string = atom_doc.css("html body div div #js-repo-pjax-container div div #readme div article.markdown-body.entry-content table tbody").text
 		data = DataFormatter.new.create_data_array_from_scraper(shortcut_string)
 	end
