7daa38aa12c476380e2e06f678732c4eeaf5b445
atxrenegade <harleighabel@gmail.com>
2018-10-01 15:52:03 -0500

    remove DataFormatter#create_object_hash

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -1,26 +1,8 @@
 class DataFormatter
-	def initialize
-	end
-
+	
 	def create_data_array_from_scraper(data)
 		shortcut_array = data.split("Block Travel").first.split("\n")
 		shortcut_array = shortcut_array.reject { |item| item.nil? || item == ''}.each_slice(5).to_a
 		binding.pry
-		#objects hash
-	end
-
-	def object_hash_from_array
-
-		@object_hash = []
-		@shortcut_array.each do |atom_array|
-			#how can I create a Hash with the following values:
-
-			shortcut["name"] = atom_array[0]
-			shortcut["mac"] = atom_array[1]
-			shortcut["windows"] = atom_array[2]
-			shortcut["linux"] = atom_array[3]
-			shortcut["description"] = atom_array[4]
-			@object_hash << shortcut
-		end
 	end
 end
