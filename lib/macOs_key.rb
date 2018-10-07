b0f664337eb34d364fd02d6b8c69f6d93684ae24
atxrenegade <harleighabel@gmail.com>
2018-10-06 22:49:46 -0500

    add methods to MacOS class

--- lib/macOs_key.rb
+++ lib/macOs_key.rb
@@ -1,8 +1,42 @@
 class MacOSKey
-	attr_accessor :key_seq
+	@all
 
-	def initialize(key)
-		@key_seq = key
+	attr_accessor :description, :shortcut, :key_seq
+
+	def initialize(key_seq)
+		@key_seq = key_seq
+		@shortcuts = []
 	end
-	
+
+	def display
+		puts "MacOS Key: \n\t #{self}\n"
+	end
+
+	def search_for_key(search_phrase)
+
+	end
+
+	def create_key(key_seq)
+		key = MacOS.new(key_seq)
+		key.save
+		key
+	end
+
+	def list_all_by_key
+		self.all.each do |key|
+			puts "MacOS Key: #{self.key_seq}:"
+			puts "Shortcut Name: #{self.shortcut.name}\n"
+			puts "Linux Key: #{self.shortcut.mac_key}\n"
+			puts "Windows Key: #{self.shortcut.windows_key}\n\n\n"
+		end
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
 end
