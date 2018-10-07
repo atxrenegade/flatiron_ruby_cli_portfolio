a7b1950c8ae3d0e015a40f443150555234db51f0
atxrenegade <harleighabel@gmail.com>
2018-10-07 10:04:34 -0500

    remove old Command class

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -2,7 +2,7 @@ class DataFormatter
 	def create_data_array_from_scraper(data)
 		shortcut_array = data.split("Block Travel").first.split("\n")
 		shortcut_array = shortcut_array.reject { |item| item.nil? || item == ''}.each_slice(5).to_a
-		#binding.pry
+		binding.pry
 		create_array_of_hashed_attributes(shortcut_array)
 	end
 
