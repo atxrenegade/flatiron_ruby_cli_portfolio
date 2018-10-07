44f984d9f2dd6311972b89f4da6f5a2ccdddb629
atxrenegade <harleighabel@gmail.com>
2018-10-06 22:47:45 -0500

    add methods to LinuxKey class

--- lib/linux_key.rb
+++ lib/linux_key.rb
@@ -1,7 +1,41 @@
 class LinuxKey
-	attr_accessor :key_seq
+	@all
 
-	def initialize(key)
-		@key_seq = key
-	end	
+	attr_accessor :description, :shortcut, key_seq
+	
+	def initialize(key_seq)
+		@key_seq = key_seq
+		@shortcuts = []
+	end
+
+	def display
+		puts "Linux Key: \n\t #{self}\n"
+	end
+
+	def search_for_key(search_phrase)
+
+	end
+
+	def create_key(key_seq)
+		key = key.new(key_seq)
+		key.save
+		key
+	end
+
+	def list_all_by_key
+		self.all.each do |key|
+			puts "Linux Key: #{self.key_seq}:"
+			puts "Shortcut Name: #{self.shortcut.name}\n"
+			puts "Mac Key: #{self.shortcut.mac_key}\n"
+			puts "Windows Key: #{self.shortcut.windows_key}\n\n\n"
+		end
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
 end
