0e39253a95c54348398aae88dc10269e6092f268
atxrenegade <harleighabel@gmail.com>
2018-10-06 20:53:27 -0500

    debug DataFormatter#create_array_of_hashed_attributes and Shortcut#initialize

--- lib/shortcut.rb
+++ lib/shortcut.rb
@@ -3,28 +3,31 @@ class Shortcut
 
 	attr_accessor :name, :description, :mac_key, :linux_key, :windows_key
 
-	def initialize(name, description, mac_key=nil, windows_key=nil, linux_key=nil )
+	def initialize(name, description=nil, mac_key=nil, windows_key=nil, linux_key=nil )
 		@name = name
-		@description = description
-		@mac_key = mac_key
-		@windows_key = windows_key
-		@linux_key = linux_key
+		self.description if description
+		self.mac_key if mac_key
+		self.windows_key if windows_key
+		self.linux_key if linux_key 	
 	end
 
 
-	def new_from_data_array(data_array)
-		#["Preferences/Settings", "cmd-,", "ctrl-,", "ctrl-,", "Opens the Preferences/Settings view"]
-		name = data_array[0]
-		mac_key_seq = data_array[1]
-		windows_key_seq = data_array[2]
-		linux_key_seq = data_array[3]
-		description_string = data_array[4]
-		new_mac_key = MacOSKey.new(mac_key_seq)
-		new_windows_key = WindowsKey.new(windows_key_seq)
-		new_linux_key = LinuxKey.new(linux_key_seq)
-		new_description = Description.new(description_string)
-		shortcut = Shortcut.new(name, mac_key_seq, windows_key_seq, linux_key_seq, description_string)
-		#create associations between new objects
+	def new_from_attribute_array(attribute_array)
+		binding.pry
+		attribute_array.each do |object|
+				#["Preferences/Settings", "cmd-,", "ctrl-,", "ctrl-,", "Opens the Preferences/Settings view"]
+			name = data_array[0]
+			mac_key_seq = data_array[1]
+			windows_key_seq = data_array[2]
+			linux_key_seq = data_array[3]
+			description_string = data_array[4]
+			new_mac_key = MacOSKey.new(mac_key_seq)
+			new_windows_key = WindowsKey.new(windows_key_seq)
+			new_linux_key = LinuxKey.new(linux_key_seq)
+			new_description = Description.new(description_string)
+			shortcut = Shortcut.new(name, mac_key_seq, windows_key_seq, linux_key_seq, description_string)
+			#create associations between new objects
+		end
 	end
 
 	def find_by_name
