cbbdfbc66745267cd3d879d2b7342e118a896d5e
atxrenegade <harleighabel@gmail.com>
2018-10-15 17:34:03 -0500

    refactor MacOSKey

--- lib/macOs_key.rb
+++ lib/macOs_key.rb
@@ -1,39 +1,17 @@
 class MacOSKey
-	@all
+	@@all
 
 	attr_accessor :description, :shortcut, :key_seq
 
 	def initialize(key_seq)
 		@key_seq = key_seq
-		@shortcuts = []
+		key_seq.save
+		#do I need to add reciprocity here?
 	end
 
 	def display
 		puts "MacOS Key: \n\t #{self}\n"
 	end
-
-	def search_for_key(search_phrase)
-
-	end
-
-	def create_key(key_seq)
-		key = MacOSKey.new(key_seq)
-		key.save
-		key
-	end
-
-	def list_all_by_key
-		self.all.each do |key|
-			puts "MacOS Key: #{self.key_seq}:"
-			puts "Shortcut Name: #{self.shortcut.name}\n"
-			puts "Linux Key: #{self.shortcut.linux_key}\n"
-			puts "Windows Key: #{self.shortcut.windows_key}\n\n\n"
-		end
-	end
-
-	def shortcut=(shortcut)
-		@shortcut = shortcut
-	end	
 
 	def save
 		@@all << self
@@ -43,4 +21,20 @@ class MacOSKey
 		@@all
 	end
 
+	def self. sort_keys_alphabetically
+
+	end
+
+	def self.list_all_by_key
+		self.all.each do |key|
+			puts "MacOS Key: #{self.key_seq}:"
+			puts "Shortcut Name: #{self.shortcut.name}\n"
+			puts "Linux Key: #{self.shortcut.linux_key}\n"
+			puts "Windows Key: #{self.shortcut.windows_key}\n\n\n"
+		end
+	end
+
+	def self.search_for_key(search_phrase)
+
+	end
 end
