98d36fb5146fc1a6079b6d0e3eef17d91b7819bf
atxrenegade <harleighabel@gmail.com>
2018-10-15 23:17:43 -0500

    add MacOS#search_by_key, #search_by_name, #search_by_number

--- lib/macOs_key.rb
+++ lib/macOs_key.rb
@@ -39,7 +39,38 @@ class MacOSKey
 		end
 	end
 
-	def self.search_for_key(search_phrase)
+	def self.search_by_name(name)
+		self.all.each do |key|
+			if key.shortcut.name == name
+				@found_key = key
+			end
+		end
+		if @found_key.nil?
+				puts "\nThat name was not found.\n"
+				puts "Please try again or choose a different name."
+			else
+				puts "\n"
+				@found_key.detail_view
+		end
+	end
+
+	def self.search_by_key(key_to_find)
+		self.all.each do |key|
+			if key.key_seq == key_to_find
+				@found_key = key
+			end
+		end
+		if @found_key.nil?
+				puts "\nThat key was not found.\n"
+				puts "Please try again or choose a different key."
+			else
+				puts "\n"
+				@found_key.detail_view
+		end
+	end
+
+	def self.search_by_number
+
 
 	end
 end
