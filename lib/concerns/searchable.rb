e26c85c5d50a64a7126958e5f73bfe4ce5f34417
atxrenegade <harleighabel@gmail.com>
2018-10-17 12:22:01 -0500

    remove self reference from class method names in Searchable module

--- lib/concerns/searchable.rb
+++ lib/concerns/searchable.rb
@@ -4,7 +4,44 @@ module Searchable
 	end
 
 	module ClassMethods
+		def search_by_name(name)
+			self.all.each do |key|
+				if key.shortcut.name == name
+					@found_key = key
+				end
+			end
+			if @found_key.nil?
+					puts "\nThat name was not found.\n"
+					puts "Please try again or choose a different name."
+				else
+					puts "\n"
+					@found_key.detail_view
+			end
+		end
 
+		def search_by_key(key_to_find)
+			self.all.each do |key|
+				if key.key_seq == key_to_find
+					@found_key = key
+				end
+			end
+			if @found_key.nil?
+					puts "\nThat key was not found.\n"
+					puts "Please try again or choose a different key."
+				else
+					puts "\n"
+					@found_key.detail_view
+			end
+		end
 
+		def search_by_number(number)
+			counter = 0
+			self.all.each do |key|
+				counter += 1
+				if counter.to_s == number
+					key.detail_view
+				end
+			end
+		end
 	end
 end
