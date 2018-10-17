fb5c6912cd7fedac21679ecff16ae48db59540f9
atxrenegade <harleighabel@gmail.com>
2018-10-17 17:35:48 -0500

    add comments to explain DataFormatter#create_data_array_from_scraper

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -1,12 +1,24 @@
 class DataFormatter
 	def create_data_array_from_scraper(data)
+		#remove uneccessary data from array
 		shortcut_array = data.split("Block Travel").first.split("\n")
+
+		#remove blank strings
 		shortcut_array = shortcut_array.reject { |item| item.nil? || item == ''}
+
+		#find index number of first set of empty table values
 		insert_location = shortcut_array.index("ctrl-shift-o")
+		#insert n/a values into array
    		revised_array = shortcut_array.insert(insert_location + 1, "n/a", "n/a")
+
+		#find second location of empty table values
 		insert_location = shortcut_array.index("alt-cmd-s")
+		#insert n/a values into array
 		revised_array = shortcut_array.insert(insert_location + 1, "n/a", "n/a")
+
+		#split array to sub arrays for object hashes
 		revised_array = revised_array.each_slice(5).to_a
+
 		create_array_of_hashed_attributes(revised_array)
 	end
 
